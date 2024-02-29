bcb99ad74faf4ad463350acf2baf799cfa88ae69
thuchede <huchede.thomas@gmail.com>
2024-02-29 20:04:53 +0800

    add sample test case

--- rest_api/src/api/handlers/hello.rs
+++ rest_api/src/api/handlers/hello.rs
@@ -3,3 +3,13 @@ use axum::http::StatusCode;
 pub async fn hello() -> Result<String, StatusCode> {
     Ok("\nHello world!\n\n".to_string())
 }
+
+
+mod tests {
+    use crate::api::handlers::hello::hello;
+
+    #[tokio::test]
+    async fn test_hello() {
+        assert_eq!(hello().await.unwrap(), "\nHello world!\n\n".to_string())
+    }
+}
