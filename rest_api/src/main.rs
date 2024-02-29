90e6a31cd1c7b75783e0b1a5bd1bc5caa787e072
thuchede <huchede.thomas@gmail.com>
2024-02-29 19:21:34 +0800

    step 1

--- rest_api/src/main.rs
+++ rest_api/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, Command};
+use dotenv::dotenv;
+
+pub fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let command = Command::new("Book Rest API")
+        .version("1.0")
+        .author("Thomas Huchedé <thomas.huchede@zenika.com>")
+        .about("A sample application to experiment with Rust-based microservices")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Configuration file location")
+                .default_value("config.json"),
+        );
+
+    let _matches = command.get_matches();
+
+    Ok(())
 }
